3be0105dc99336b0c38e1fa08ee26516a40c2c02
rachelsteiner91 <127536637+rachelsteiner91@users.noreply.github.com>
2023-06-16 12:14:20 -0400

    patch and delete

--- server/app.py
+++ server/app.py
@@ -21,7 +21,8 @@ class Plants(Resource):
     def get(self):
         plants = [plant.to_dict() for plant in Plant.query.all()]
         return make_response(jsonify(plants), 200)
-
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# POST #
     def post(self):
 
         data = request.get_json()
@@ -38,12 +39,36 @@ class Plants(Resource):
         return make_response(new_plant.to_dict(), 201)
 
 api.add_resource(Plants, '/plants')
-
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 class PlantByID(Resource):
 
     def get(self, id):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
+
+# PATCH #
+## make a PATCH request with an object in the body that updates one plant ## return the updated plant in the response
+    def patch(self, id):
+        data = request.get_json()
+        plant = Plant.query.filter_by(id=id).first() 
+
+        for attr in data:
+            setattr(plant, attr, data[attr])
+        
+        db.session.add(plant)
+        db.session.commit()
+
+        return make_response(plant.to_dict(), 200)
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ # DELETE #
+ # the DELETE request should delete one plant from the data base
+ # return an empty string aka '' with the status code 204 (no content)to indicate a successful deletion    
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        db.session.delete(plant)
+        db.session.commit()
+
+        return make_response('', 204)
 
 api.add_resource(PlantByID, '/plants/<int:id>')
         
